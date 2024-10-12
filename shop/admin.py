cd74670fd74c96e8969f65dde3d95d6e432ac387
Artem Simonyan <artemsim2006@mail.ru>
2024-10-12 13:29:24 +0300

    add models in admin site

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
-
 # Register your models here.
+
+from django.contrib import admin
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    # атрибут prepopulated_fields значение которых устанавливается автоматически с 
+    # использованием значения других полей
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'available',
+                    'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
