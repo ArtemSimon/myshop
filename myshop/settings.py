10031881c05ba7b4b17595626fe7c2c6d7bac8a5
artem <artemsim2006@mail.ru>
2024-10-25 21:33:40 +0300

    change db postgres

--- myshop/settings.py
+++ myshop/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     "debug_toolbar",
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
@@ -83,10 +84,21 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'myshop',
+    'USER': 'artem',
+    'PASSWORD': '123',
+# 'HOST': 'localhost',
+# 'PORT': '5432', # default PostgreSQL port
     }
 }
 
