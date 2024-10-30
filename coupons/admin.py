1e4809eab1b0d02fcf19732d1a226608c87b7fca
artem <artemsim2006@mail.ru>
2024-10-30 12:46:23 +0300

    fixed bags

--- coupons/admin.py
+++ coupons/admin.py
@@ -7,6 +7,6 @@ from .models import Coupon
 class CouponAdmin(admin.ModelAdmin):
     list_display = ['code', 'valid_from', 'valid_to',
 'discount', 'active']
-list_filter = ['active', 'valid_from', 'valid_to']
-search_fields = ['code']
+    list_filter = ['active', 'valid_from', 'valid_to']
+    search_fields = ['code']
 
