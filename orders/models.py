ae6653ba65d7ad5c12ede864d0cec5dec016e827
artem <artemsim2006@mail.ru>
2024-11-01 16:24:46 +0300

    change fixed

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,10 @@
 # Create your models here.
 
 from django.db import models
+from decimal import Decimal
+from django.core.validators import MinValueValidator, \
+                                    MaxValueValidator
+from coupons.models import Coupon
 from shop.models import Product
 
 class Order(models.Model):
@@ -13,18 +17,35 @@ class Order(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
-
+    coupon = models.ForeignKey(Coupon,
+                               related_name='orders',
+                               null=True,
+                               blank=True,
+                               on_delete=models.SET_NULL)
+    discount = models.IntegerField(default=0,
+                                   validators=[MinValueValidator(0),
+                                               MaxValueValidator(100)])
     class Meta:
         ordering = ['-created']
         indexes = [
             models.Index(fields=['-created']),
         ]
 
-    def __str__(self):
+    def __str__(self): 
         return f'Order {self.id}'
     
     def get_total_cost(self):
+        total_cost = self.get_total_cost_before_discount()
+        return total_cost - self.get_discount()
+    
+    def get_total_cost_before_discount(self):
         return sum(item.get_cost() for item in self.items.all())
+    
+    def get_discount(self):
+        total_cost = self.get_total_cost_before_discount()
+        if self.discount:
+            return total_cost * (self.discount / Decimal(100))
+        return Decimal(0)
     
 class OrderItem(models.Model):
     order = models.ForeignKey(Order,
