aba60a344d5d5245fcf3e6984bea4a48b9f4fc3c
artem <artem@localhost.localdomain>
2024-11-06 16:37:41 +0300

    change views for recommended products

--- cart/views.py
+++ cart/views.py
@@ -6,6 +6,7 @@ from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
 from coupons.forms import CouponApplyForm
+from shop.recommender import Recommender
 
 @require_POST
 def cart_add(request, product_id):
@@ -34,5 +35,15 @@ def cart_detail(request):  # отоюражение содержимого ко�
                             'quantity': item['quantity'],
                             'override': True})
     coupon_apply_form = CouponApplyForm()
+
+    r = Recommender()
+    cart_products = [item['product'] for item in cart]
+    if(cart_products):
+        recommended_products = r.suggest_products_for(
+                                cart_products,
+                                max_results=4)
+    else:
+        recommended_products = []
     return render(request, 'cart/detail.html', {'cart': cart,
-                                                "coupon_apply_form": coupon_apply_form})
+                                                "coupon_apply_form": coupon_apply_form,
+                                                'recommended_products': recommended_products})
